1a9c2ae0f45f153860df25034c9e3d6e2a97a9eb
Ali İlteriş Keskin <ilteriskesk20@gmail.com>
2018-03-04 23:16:21 +0300

    Türkçe karakter düzeltmeleri yapıldı

--- Soru13.cpp
+++ Soru13.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
-#include<string>
+#include <string>
 
 using namespace std;
 
 int main() {
 
     int var = 0;
-    char *dizi[10] ={"Selamet","Hayri","Fikret","İlteriş","Enes","Burak","Muhammed","Meva","Akif","Edison"};
+    char *dizi[10] ={"Selamet","Hayri","Fikret","Ilteris","Enes","Burak","Muhammed","Meva","Akif","Edison"};
 
     char ad[10];
 
-    cout<<"Bir ad giriniz:";
-    cin>>ad;
+    cout << "Bir ad giriniz:";
+    cin >> ad;
 
     for (int i = 0; i <10; ++i) {
 
         if(strcmp(dizi[i],ad)==0) //strcmp fonksiyonu girilen iki string değerinin aynı olup olmadığını kontrol eder.
                                 // eğer iki string aynı ise geriye 0 değerini döndürür. Diğer durumlarda ise 1 değerini dondurur.
 
-            var =1;
+            var = 1;
     }
-    if(var==1){
-        cout<<"Girilen ad dizide var\n";
+    if(var == 1){
+        cout << "Girilen ad dizide var\n";
     }else{
-        cout<<"Girilen ad dizide yok"<<endl;
+        cout << "Girilen ad dizide yok" << endl;
     }
 
 
